780b1564c917a919f613f77cad4a14c0083d0c5d
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-25 02:52:04 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -2,10 +2,12 @@ require 'pry'
 
 def starts_with_a_vowel?(word)
  word.downcase.scan(/\b[aeiou]/) != []
+  # word.match(/^[aeiouAEIOU]\w+/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
 text.scan(/\bun\w+ing\b/)
+# text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
@@ -13,7 +15,8 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.scan(/^[A-Z].+\b[\,\.\?\!]\z/) != []
+text.scan(/^[A-Z].+\b[,\.?!]\z/) != []
+# text.match(/^[A-Z].+[\.!?]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
